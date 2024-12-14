04a60e6405fd1a5327fe2361228593f8afefd222
Jay Pandey <jaypandey7421@gmail.com>
2024-12-14 23:04:31 +0530

    Made it functional

--- server02.js
+++ server02.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const loginRouter = require('./routes/login');
+const homeRouter = require('./routes/home');
 
 const app = express();
 
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended: false}));       
-app.use('/', loginRouter);
+app.use('/', homeRouter);
+app.use('/login', loginRouter);
 
 
 app.listen(PORT,()=> console.log("app is up and running!"));
