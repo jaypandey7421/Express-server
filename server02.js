e59e48fde926f3600b76e8b789ea41823f9586ea
Jay Pandey <jaypandey7421@gmail.com>
2024-12-14 14:05:18 +0530

    Added seperate router

--- server02.js
+++ server02.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const loginRouter = require('./routes/login');
 
 const app = express();
 
 const PORT = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));       // parse body data
-app.use(express.static(path.join(__dirname, 'public'))); // serve static file
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));       
+app.use('/', loginRouter);
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-// app.get('/login',(req, res)=>{
-//     res.sendFile(path.join(__dirname, 'public', 'login.html'));
-//     console.log('log-in handler');
-// });
-
-app.post('/login',(req, res)=>{
-    let email = req.body.email;
-    let password = req.body.password;
-    console.log("Post executed.");
-    res.send(`Email: ${email} Password: ${password}`);
-})
 
 app.listen(PORT,()=> console.log("app is up and running!"));
